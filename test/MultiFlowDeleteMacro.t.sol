f76d304a111c9ad936f4c96341f3ee15ef8b26ae
didi <git@d10r.net>
2026-02-09 11:22:26 +0100

    added test

---
+++ test/MultiFlowDeleteMacro.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { FoundrySuperfluidTester } from "@superfluid-finance/ethereum-contracts/test/foundry/FoundrySuperfluidTester.sol";
+import { MultiFlowDeleteMacro } from "../src/MultiFlowDeleteMacro.sol";
+
+contract MultiFlowDeleteMacroTest is FoundrySuperfluidTester {
+    MultiFlowDeleteMacro internal multiFlowDeleteMacro;
+
+    constructor() FoundrySuperfluidTester(3) {}
+
+    function setUp() public override {
+        super.setUp();
+        multiFlowDeleteMacro = new MultiFlowDeleteMacro();
+    }
+
+    function test_buildBatchOperations_smoke() public {
+        address[] memory receivers = new address[](2);
+        receivers[0] = bob;
+        receivers[1] = carol;
+        bytes memory params = multiFlowDeleteMacro.getParams(superToken, receivers);
+        assertEq(multiFlowDeleteMacro.buildBatchOperations(sf.host, params, admin).length, 2);
+    }
+}
